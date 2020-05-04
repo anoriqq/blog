bd4f9212e327dd6c9f387bb0fba8f184bacb991e
anoriqq <shota.yoshikawa@anoriqq.com>
2020-05-04 22:24:12 +0900

    chore: change color

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -25,11 +25,11 @@ export interface Palette {
 export const palette = {
   darkBase: '0,0,0',
   darkMain: '255,255,255',
-  darkAccent: '79, 249, 255',
+  darkAccent: '0, 122, 204',
 
   lightBase: '255,255,255',
   lightMain: '0,0,0',
-  lightAccent: '79, 249, 255',
+  lightAccent: '0, 122, 204',
 } as const;
 
 export const GlobalStyles = createGlobalStyle<{ theme: Theme }>`
