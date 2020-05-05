4aacf54e919879babf9351b7fa31abac413d53a8
anoriqq <shota.yoshikawa@anoriqq.com>
2020-05-05 20:51:02 +0900

    chore: add custom local plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-remark-headers-link`,
         ],
       },
     },
